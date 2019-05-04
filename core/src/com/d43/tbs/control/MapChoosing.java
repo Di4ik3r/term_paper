a0f6bb5e8fcb2d658a27f97ff42302c1df0713fe
Di4ik3r <di4iker@gmail.com>
2019-05-04 14:40:44 +0300

    adasdasd

--- core/src/com/d43/tbs/control/MapChoosing.java
+++ core/src/com/d43/tbs/control/MapChoosing.java
@@ -87,6 +87,11 @@ public class MapChoosing extends MapHandler {
 				else {
 					this.pickedUnit.getBounds().setPosition(1366, 768);
 					//adding some comments
+					
+					
+					
+					
+					//adding some comments
 				}
 			}
 			Unit newUnit = unit.clone();
