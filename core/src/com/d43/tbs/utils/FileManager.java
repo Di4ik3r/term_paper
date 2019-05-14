7f347b99b9ef5b2a91c94220579f382cccb7401f
Di4ik3r <di4iker@gmail.com>
2019-05-14 13:45:21 +0300

    added folder check(if it isn't created - it will create)

--- core/src/com/d43/tbs/utils/FileManager.java
+++ core/src/com/d43/tbs/utils/FileManager.java
@@ -13,125 +13,126 @@ import com.badlogic.gdx.Gdx;
 import com.d43.tbs.model.unit.Unit;
 
 public class FileManager {
-	
+
 	public FileManager() {
-		
+
 	}
-	
+
 	public void deleteFile(String name) {
 		File file = new File("saves//" + name);
 		file.delete();
 	}
-	
+
 	public ArrayList<String> getSaves() {
 		ArrayList<String> names = new ArrayList<String>();
-		
-		File folder = new File("saves//");
-		File[] listOfFiles = folder.listFiles();
-		
-		for(int i = 0; i < listOfFiles.length; i++) {
-			for(int j = 0; j < listOfFiles.length - 1; j++) {
-				if(listOfFiles[j].lastModified() < listOfFiles[j + 1].lastModified())
-				{
-					File f = listOfFiles[j];
-					listOfFiles[j] = listOfFiles[j + 1];
-					listOfFiles[j + 1] = f;
+		try {
+			File folder = new File("saves//");
+			File[] listOfFiles = folder.listFiles();
+
+			for (int i = 0; i < listOfFiles.length; i++) {
+				for (int j = 0; j < listOfFiles.length - 1; j++) {
+					if (listOfFiles[j].lastModified() < listOfFiles[j + 1].lastModified()) {
+						File f = listOfFiles[j];
+						listOfFiles[j] = listOfFiles[j + 1];
+						listOfFiles[j + 1] = f;
+					}
 				}
 			}
+
+			for (int i = 0; i < listOfFiles.length; i++) {
+				if (listOfFiles[i].isFile())
+					names.add(listOfFiles[i].getName());
+			}
+		} catch (Exception ex) {
+
 		}
 
-		for (int i = 0; i < listOfFiles.length; i++) {
-		  if (listOfFiles[i].isFile())
-			  names.add(listOfFiles[i].getName());
-//		  {
-//		    System.out.println("File " + listOfFiles[i].getName());
-//		  } else if (listOfFiles[i].isDirectory()) {
-//		    System.out.println("Directory " + listOfFiles[i].getName());
-//		  }
-		}
-		
-//		Collections.sort(names, new Comparator<String>() {
-//	        @Override
-//	        public int compare(String name1, String name2)
-//	        {
-//	            int n1 = Integer.valueOf(name1.split("\\.")[0]);
-//	            int n2 = Integer.valueOf(name2.split("\\.")[0]);
-//	            return n1 > n2 ? 1 : n1 < n2 ? -1 : 0;
-//	        }
-//	    });
-		
 		return names;
 	}
-	
+
 	public ArrayList<Unit> read(String name) {
 		try {
 			ArrayList<Unit> units = new ArrayList<Unit>();
 			FileInputStream fis = new FileInputStream("saves//" + name);
 			ObjectInputStream oin = new ObjectInputStream(fis);
-			units = (ArrayList<Unit>)oin.readObject();
+			units = (ArrayList<Unit>) oin.readObject();
 			oin.close();
 			return units;
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			Gdx.app.log("file read", ex.toString());
 			return null;
 		}
 	}
-	
+
+	public void folderProcess() {
+		if (!folderExitsts()) {
+			File file = new File("saves//");
+			file.mkdir();
+		}
+	}
+
+	public boolean folderExitsts() {
+		File file = new File("saves//");
+		return file.exists();
+	}
+
 	public void save(ArrayList<Unit> units) {
 		try {
+			folderProcess();
+			
 			increaseSaveCount();
 			String name = Integer.toString(getSaveCount());
 			FileOutputStream fos = new FileOutputStream("saves//" + name + ".out");
 			ObjectOutputStream os = new ObjectOutputStream(fos);
-		  	os.writeObject(units);
+			os.writeObject(units);
 			os.flush();
 			os.close();
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			Gdx.app.log("file read", ex.toString());
-			
+
 			return;
 		}
 	}
-	
+
 	private int getSaveCount() {
 		try {
 			FileInputStream fis = new FileInputStream("saveCount.out");
 			ObjectInputStream oin = new ObjectInputStream(fis);
-			int count = (Integer)oin.readObject();
+			int count = (Integer) oin.readObject();
 			oin.close();
 			return count;
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			Gdx.app.log("file read", ex.toString());
 			return 0;
 		}
 	}
-	
+
 	private void increaseSaveCount() {
-		if(getSaveCount() >= 53)
+		if (getSaveCount() >= 53)
 			setToZeroSaveCount();
 		try {
 			int count = getSaveCount() + 1;
 			FileOutputStream fos = new FileOutputStream("saveCount.out");
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
-		  	oos.writeObject(count);
+			oos.writeObject(count);
 			oos.flush();
 			oos.close();
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			Gdx.app.log("file write", ex.toString());
 			ex.printStackTrace();
 			return;
 		}
 	}
-	
+
 	private void setToZeroSaveCount() {
 		try {
 			int count = 0;
 			FileOutputStream fos = new FileOutputStream("saveCount.out");
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
-		  	oos.writeObject(count);
+			oos.writeObject(count);
 			oos.flush();
 			oos.close();
-		} catch(Exception ex) {
+		} catch (Exception ex) {
 			Gdx.app.log("file write", ex.toString());
 			ex.printStackTrace();
 			return;
